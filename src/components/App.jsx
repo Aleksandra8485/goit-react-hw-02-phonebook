640ea9c3946d52ab2dbaf5f8d0d4eaa85d1eafff
Aleksandra <a.walczak1984@gmail.com>
2023-06-15 22:14:47 +0200

    number is nott adding to contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,90 @@
 import React, { useState } from 'react';
-import ContactForm from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
 
 function App() {
   const [state, setState] = useState({
     contacts: [],
+    filter: '',
     name: '',
     number: '',
   });
 
-  const addContact = newContact => {
-    setState({
-      ...state,
-      contacts: [...state.contacts, newContact],
-    });
+  const handleNameChange = event => {
+    setState({ ...state, name: event.target.value });
   };
 
   const handleNumberChange = event => {
-    setState({
-      ...state,
-      number: event.target.value,
-    });
+    setState({ ...state, number: event.target.value });
   };
+
+  const handleFilterChange = event => {
+    setState({ ...state, filter: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (state.name.trim() !== '' && state.number.trim() !== '') {
+      const newContact = {
+        id: nanoid(),
+        name: state.name,
+        number: state.number,
+      };
+
+      setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+        filter: '',
+        name: '',
+        number: '',
+      }));
+    }
+  };
+
+  const filteredContacts = state.contacts.filter(contact =>
+    contact.name.toLowerCase().includes(state.filter.toLowerCase())
+  );
 
   return (
     <div>
-      <h1>Phonebook</h1>
-      <ContactForm
-        addContact={addContact}
-        number={state.number}
-        handleNumberChange={handleNumberChange}
-      />
+      <h1>Książka telefoniczna</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Imię i nazwisko:
+          <input
+            type="text"
+            value={state.name}
+            onChange={handleNameChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' ][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Imię i nazwisko mogą zawierać tylko litery, apostrof, myślnik i spacje. Na przykład Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Numer telefonu:
+          <input
+            type="tel"
+            value={state.number}
+            onChange={handleNumberChange}
+            pattern="\+?\d{1,4}?\s?\(?\d{1,3}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}"
+            title="Numer telefonu musi zawierać tylko cyfry i może zawierać spacje, myślniki, nawiasy i może zaczynać się od +"
+            required
+          />
+        </label>
+        <button type="submit">Dodaj kontakt</button>
+      </form>
 
       <div>
-        <h2>Contacts</h2>
+        <h2>Lista kontaktów:</h2>
+        <input
+          type="text"
+          placeholder="Wyszukaj kontakt..."
+          value={state.filter}
+          onChange={handleFilterChange}
+        />
         <ul>
-          {state.contacts.map(contact => (
-            <li key={contact.id}>
-              {contact.name} - {contact.number}
-            </li>
+          {filteredContacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
           ))}
         </ul>
       </div>
